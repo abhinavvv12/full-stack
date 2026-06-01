19b561caba097b316229d49aa6e24f562f99e1b7
Abhinav <abhinav.r1296@gmail.com>
2026-06-01 22:28:23 +0530

    In-Built Objects

---
+++ Java Script 10/index.js
@@ -0,0 +1,23 @@
+// // Math Obj
+// console.log('Jimmy');
+// console.log(Math.PI);
+// console.log(Math.max(1,2,3,4,5,3,4,8,90,7,6,5,33,4,));
+// console.log(Math.min(1,2,3,4,5,3,4,8,90,7,6,5,33,4,));
+// console.log(Math.round(1.4));
+// console.log(Math.floor(1.9));
+// console.log(Math.ceil(1.9));
+// console.log(Math.abs(-5));
+// console.log(Math.random());
+// console.log(Math.sqrt(5));
+// console.log(Math.pow(2,10));
+
+
+// // Date Obj
+// let curr = new Date();
+// let date = new Date('June 20 1998 07:15');
+// console.log(date);
+// let newDate = new Date(1998, 7, 20, 7);
+// console.log( curr );
+// newDate.getFullYear();
+// console.log(newDate.getDay());
+
