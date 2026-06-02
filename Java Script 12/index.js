76f0f4ae981aa1d8eda097453c666eac0fe372e6
Abhinav <abhinav.r1296@gmail.com>
2026-06-02 10:36:12 +0530

    Error Handling

---
+++ Java Script 12/index.js
@@ -0,0 +1,30 @@
+// Error Handling in JS
+// x = 90;
+
+// try {
+//     console.log('Try block starts here');
+//     console.log(x);
+//     console.log('try block ends here');
+// }
+// catch(err) {
+//     // define krte h, error ke sath aap kya krna chahte h
+//     // retry logic
+//     // fa;;back mechanism
+//     // logging
+//     // custom error
+//     console.log('I am inside catch block');
+//     console.log('your error is here');
+// }
+// finally {
+//     console.log('I will run everytime, even though error occurs or not');
+// 
+
+
+// // let's create a custom error
+// try {
+//     console.log(x);
+// }
+// catch(err) {
+//     throw new Error('Bhai pehle declare kro, fir print karo'); 
+
+// }
