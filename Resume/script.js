696d43fa60e8cee617fbc55819c9400f417357ea
Abhinav <abhinav.r1296@gmail.com>
2026-06-19 22:30:28 +0530

    updated recipe Project

---
+++ Resume/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("mousemove",(e)=>{
+
+document.documentElement.style.setProperty(
+"--x",
+e.clientX+"px"
+);
+
+document.documentElement.style.setProperty(
+"--y",
+e.clientY+"px"
+);
+
+});
